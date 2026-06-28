aaf3afa895bbbf1b36f18364c12b6d7e313ba756
Alroba25 <mohamedadelar23@gmail.com>
2026-06-28 22:59:16 +0300

    Handel App To Deployment

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
+
 const express = require("express");
 const connectDB = require("./Config");
 const routes = require("./Routes/index.route");
 const cors = require("cors");
+
 const app = express();
+
 app.use(cors());
+
 app.use(express.json({ limit: "50mb" }));
 
 app.use(
@@ -13,8 +17,13 @@ app.use(
     extended: true,
   }),
 );
+
 app.use("/", routes);
+
 connectDB();
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
